f0c7fe271e48fec52b8f0798bc7374d93c7b81bf
Artem Izmailov <temalinho@gmail.com>
2023-05-04 20:36:39 +0300

    connect db

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,13 +1,16 @@
 import { Injectable } from '@nestjs/common';
-import {InjectModel} from 'nestjs-typegoose'
+import { ModelType } from '@typegoose/typegoose/lib/types';
+import { InjectModel } from 'nestjs-typegoose';
+import { UserModel } from 'src/user/user.model';
 
 @Injectable()
 export class AuthService {
- constructor(@InjectModel(UserModel) private readonly userModel: ModelType<UserModel>) {
-
- }
+  constructor(
+    @InjectModel(UserModel) private readonly userModel: ModelType<UserModel>,
+  ) {}
 
   async register(dto: any) {
-    return this.
+    const newUser = new this.userModel(dto);
+    return newUser.save();
   }
 }
