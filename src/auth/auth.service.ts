1f88ba3668d7230524cb9bd81b1d243c3f0a55a8
Artem Izmailov <temalinho@gmail.com>
2023-05-04 14:27:29 +0300

    add auth and user folder with co, mo, s without logic

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@nestjs/common';
+import {InjectModel} from 'nestjs-typegoose'
+
+@Injectable()
+export class AuthService {
+ constructor(@InjectModel(UserModel) private readonly userModel: ModelType<UserModel>) {
+
+ }
+
+  async register(dto: any) {
+    return this.
+  }
+}
