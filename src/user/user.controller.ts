c935737d0606469724b9f86616b8581f3b90f978
Artem Izmailov <temalinho@gmail.com>
2023-05-07 14:10:09 +0300

    add CRUD user profile, get one and all users

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import {
+	Body,
+	Controller,
+	Get,
+	HttpCode,
+	Param,
+	Put,
+	UsePipes,
+	ValidationPipe,
+	Delete,
+	Query,
+} from '@nestjs/common';
 import { Auth } from 'src/auth/decorators/auth.decorator';
 import { UserService } from './user.service';
 import { User } from './decorators/user.decorator';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { IdValidationPipe } from 'src/pipes/id.validation.pipe';
 
 @Controller('user')
 export class UserController {
@@ -11,5 +24,38 @@ export class UserController {
 	@Auth('admin')
 	async getProfile(@User('_id') _id: string) {
 		return this.userService.byId(_id);
+	}
+
+	@UsePipes(new ValidationPipe())
+	@Put('profile')
+	@HttpCode(200)
+	@Auth()
+	async updateProfile(@User('_id') _id: string, @Body() dto: UpdateUserDto) {
+		return this.userService.updateProfile(_id, dto);
+	}
+
+	@Get('count')
+	@Auth('admin')
+	async getCountUsers() {
+		return this.userService.getCount();
+	}
+
+	@Get()
+	@Auth('admin')
+	async getUsers(@Query('searchTerm') searchTerm?: string) {
+		return this.userService.getAll(searchTerm);
+	}
+
+	@Get(':id')
+	@Auth('admin')
+	async getUser(@Param('id', IdValidationPipe) id: string) {
+		return this.userService.byId(id);
+	}
+
+	@Delete(':id')
+	@HttpCode(200)
+	@Auth('admin')
+	async deleteUser(@Param('id', IdValidationPipe) id: string) {
+		return this.userService.delete(id);
 	}
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,6 +2,8 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from 'nestjs-typegoose';
 import { UserModel } from './user.model';
 import { ModelType } from '@typegoose/typegoose/lib/types';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { genSalt, hash } from 'bcryptjs';
 
 @Injectable()
 export class UserService {
@@ -15,5 +17,44 @@ export class UserService {
 		return user;
 	}
 
-	async updateProfile() {}
+	async updateProfile(_id: string, dto: UpdateUserDto) {
+		const user = await this.byId(_id);
+		const candidate = await this.userModel.findOne({ email: dto.email });
+
+		if (candidate && String(_id) !== String(candidate._id))
+			throw new NotFoundException('Email busy');
+
+		if (dto.password) {
+			const salt = await genSalt(9);
+			user.password = await hash(dto.password, salt);
+		}
+		user.email = dto.email;
+		if (dto.isAdmin || dto.isAdmin === false) user.isAdmin = dto.isAdmin;
+
+		await user.save();
+		return;
+	}
+
+	async getCount() {
+		return this.userModel.find().count().exec();
+	}
+
+	async getAll(searchTerm?: string) {
+		let options = {};
+
+		if (searchTerm) {
+			options = {
+				$or: [{ email: new RegExp(searchTerm, 'i') }],
+			};
+		}
+		return this.userModel
+			.find(options)
+			.select('-password -updatedAt -__v')
+			.sort({ createdAt: 'desc' })
+			.exec();
+	}
+
+	async delete(id: string) {
+		return this.userModel.findByIdAndDelete(id).exec();
+	}
 }
