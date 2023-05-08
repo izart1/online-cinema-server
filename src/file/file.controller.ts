8f94768f1a9545a14cf723d33294b5d5eaa7085a
Artem Izmailov <temalinho@gmail.com>
2023-05-08 11:01:08 +0300

    add file upload

--- src/file/file.controller.ts
+++ src/file/file.controller.ts
@@ -1,4 +1,27 @@
-import { Controller } from '@nestjs/common';
+import {
+	Controller,
+	HttpCode,
+	Post,
+	Query,
+	UploadedFile,
+	UseInterceptors,
+} from '@nestjs/common';
+import { FileService } from './file.service';
+import { Auth } from 'src/auth/decorators/auth.decorator';
+import { FileInterceptor } from '@nestjs/platform-express';
 
 @Controller('file')
-export class FileController {}
+export class FileController {
+	constructor(private readonly fileService: FileService) {}
+
+	@Post()
+	@HttpCode(200)
+	@Auth('admin')
+	@UseInterceptors(FileInterceptor('file'))
+	async uploadFile(
+		@UploadedFile() file: Express.Multer.File,
+		@Query('folder') folder?: string,
+	) {
+		return this.fileService.saveFiles([file], folder);
+	}
+}
